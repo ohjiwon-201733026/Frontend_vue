63dab3ef547943909bc1ef9acd35395cc37872ba
Oh, Jiwon <ojw970725@gmail.com>
2020-05-04 09:40:32 +0900

    중간고사2

--- public/Test/mid4.js
+++ public/Test/mid4.js
@@ -1,6 +1,21 @@
-function sum(...a){
+function sum(a){
     let temp=0;
     for(let i=0;i<a.length;++i)
-        
+         temp+=a[i];
+        return temp;
+}
+
+function test_sum(callback){
+    let array=[];
+    for(let i=0;i<5;++i){
+        array[i]=Math.floor(Math.random()*(10-0+1)+0);
+    }
+    console.log(array);
+    let result=callback(array);
+    console.log(result);
     
+}
+
+for(let i=0;i<5;++i){
+    test_sum(sum);
 }
