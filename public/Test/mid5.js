b8bb207f3b910f4cf71f5c5d3a438c51bc31c6bf
Oh, Jiwon <ojw970725@gmail.com>
2020-05-04 10:00:44 +0900

    중간고사3

---
+++ public/Test/mid5.js
@@ -0,0 +1,9 @@
+let array=[];
+let MIN=1,MAX=100;
+for(let i=0;i<100;++i){
+    array[i]=Math.floor(Math.random()*(MAX-MIN+1)+MIN);
+}
+let even=array.filter((e)=>e%2==0);
+let sum=even.reduce((a,b)=>a+b);
+console.log(even);
+console.log(sum);
